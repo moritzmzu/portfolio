41e5c6db8f8faa17d4bf10d128fb303126d9774e
Moritz <moritzmzu@gmail.com>
2024-01-09 22:12:12 +0100

    added tailwind and some stuff for the hero and satoshi

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
